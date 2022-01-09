487cc66d68360b584b16289fc0cb75d31f0994cc
Mrunal Gaurkar <gaurkarmrunal27@gmail.com>
2022-01-09 09:52:18 +0530

    Added java demo application files

---
+++ src/main/java/org_example/Java_jenkins_in_docker/Main.java
@@ -0,0 +1,16 @@
+package org_example.Java_jenkins_in_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( EvenOrOdd(122) );
+    }
+    public static boolean EvenOrOdd(int number) {
+    	return number % 2==0;
+    }
+}
---
+++ src/test/java/org_example/Java_jenkins_in_docker/TestMain.java
@@ -0,0 +1,20 @@
+package org_example.Java_jenkins_in_docker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestMain
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void TestEven()
+    {
+        assertTrue(Main.EvenOrOdd(22));
+    }
+}
